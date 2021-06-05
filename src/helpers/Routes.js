612068e97e30f1e38264fd5129075ca58c5fc880
Katy-fry <katyvfry@gmail.com>
2021-06-05 11:55:14 -0500

    add routes and navbar and auth

---
+++ src/helpers/Routes.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import BookshelvesView from '../views/BookshelvesView';
+import BooksView from '../views/BooksView';
+import Explore from '../views/Explore';
+import Home from '../views/Home';
+import NotFound from '../views/NotFound';
+import Search from '../views/Search';
+
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  const routeChecker = (taco) => (user
+    ? (<Component {...taco} user={user} />)
+    : (<Redirect to={{ pathname: '/', state: { from: taco.location } }} />));
+
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+  user: PropTypes.any
+};
+
+export default function Routes({ user }) {
+  return (
+    <div>
+      <Switch>
+        <Route
+        exact
+        path='/'
+        component={Home}
+        />
+        <PrivateRoute
+        exact
+        path='/bookshelves'
+        user={user}
+        component={BookshelvesView}
+        />
+        <PrivateRoute
+        exact
+        path='/books'
+        user={user}
+        component={BooksView}
+        />
+        <PrivateRoute
+        exact
+        path='/explore'
+        user={user}
+        component={Explore}
+        />
+        <PrivateRoute
+        exact
+        path='/search'
+        user={user}
+        component={Search}
+        />
+        <Route
+        path='*'
+        component={NotFound}
+        />
+      </Switch>
+    </div>
+  );
+}
+
+Routes.propTypes = {
+  user: PropTypes.any
+};
---
+++ src/views/BooksView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BooksView() {
+  return (
+    <div>
+      <h1>this is the books view</h1>
+    </div>
+  );
+}
