82cbc9e2f518c1ce580b3d275cf2f91df7c3e818
Katy-fry <katyvfry@gmail.com>
2021-06-07 21:16:42 -0500

    add sort functionality and styled cards

--- src/views/BooksView.js
+++ src/views/BooksView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import BookForm from '../components/forms/bookForm';
@@ -12,11 +12,37 @@ justify-content: center;
 `;
 
 export default function BooksView({ books, setBooks, user }) {
-  console.warn(books);
+  const [creating, setCreating] = useState(false);
+  const seeForm = () => {
+    setCreating((prevState) => !prevState);
+  };
+
+  const sortBooks = () => {
+    const sortedBook = [...books];
+    sortedBook.sort((a, b) => {
+      if (a.rating > b.rating) return -1;
+      if (a.rating < b.rating) return 1;
+      return 0;
+    });
+    setBooks(sortedBook);
+  };
+
   return (
     <div>
-      <h1>this is the books view</h1>
-      <BookForm setBooks={setBooks} user={user}/>
+      <h1 className='font-mono text-center'>my books</h1>
+      <button type='button'
+      className='bg-yellow-400 hover:bg-yellow-500 text-white py-2 px-3 rounded-full'
+      onClick={seeForm}>
+        {creating ? 'close form' : '+ add a book'}
+      </button>
+
+      {creating && <BookForm setBooks={setBooks} user={user}/>}
+
+      <button type='button'
+      className='bg-red-400 hover:bg-red-500 text-white py-2 px-3 rounded-full'
+      onClick={sortBooks}>
+        sort by rating
+        </button>
       <BookContainer>
       {books.map((book) => (
         <BookCard
